d77707809325c8f7152226a499df7ddc1aa4b903
betene94 <123448598+betene94@users.noreply.github.com>
2023-02-20 11:50:11 +0100

    Add files via upload
    Arrêt si détection de logiciel métier

---
+++ logMetier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+class logMetier
+{
+    static void Main(string[] args)
+    {
+        // Vérifier si Word est en cours d'exécution
+        if (Process.GetProcessesByName("winword").Length > 0)
+        {
+            // Afficher un message d'erreur et arrêter l'exécution du programme
+            Console.WriteLine("Un logiciel métier est en cours d'exécution. Fermez-le et réessayez.");
+            return; //Si un ou plusieurs processus sont trouvés, le code affiche un message d’erreur à l’utilisateur et arrête l’exécution du programme en utilisant l’instruction « return ».
+        }
+        // Instructions de votre programme
+    }
+}
